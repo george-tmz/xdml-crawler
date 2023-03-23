edff7ea1ab057564c5a2a68fe3dde9c9d8484677
George <jobsteng@163.com>
2023-03-23 21:00:19 +0800

    简单重构爬虫算法

--- src/main/java/cn/wbombt/Main.java
+++ src/main/java/cn/wbombt/Main.java
@@ -24,10 +24,9 @@ public class Main {
     public static void main(String[] args) {
         // 待处理的链接池
         List<String> linkPool = new ArrayList<>();
-        linkPool.add("https://sina.cn/");
-//        已处理过的连接池
+        //已处理过的连接池
         Set<String> processedLinks = new HashSet<>();
-
+        linkPool.add("https://sina.cn/");
         while (!linkPool.isEmpty()) {
             //remove()函数返回被删除的元素
             String link = linkPool.remove(linkPool.size() - 1);
@@ -35,43 +34,60 @@ public class Main {
             if (link.startsWith("//")) {
                 link = "https:" + link;
             }
-//          是否处理过
+            //是否处理过
             if (processedLinks.contains(link)) {
                 continue;
             }
             // 是要处理的
-            if (link.contains("news.sina.cn") && !link.contains("passport.sina.cn") || "https://sina.cn/".equals(link)) {
-                try (CloseableHttpClient httpclient = HttpClients.createDefault()) {
-                    ClassicHttpRequest httpGet = ClassicRequestBuilder.get(link)
-                            .addHeader("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36")
-                            .build();
-                    String finalLink = link;
-                    httpclient.execute(httpGet, response -> {
-                        System.out.println(finalLink);
-                        System.out.println(response.getReasonPhrase());
-                        final HttpEntity entity1 = response.getEntity();
-                        String html = EntityUtils.toString(entity1);
-                        Document doc = Jsoup.parse(html);
-                        // 获取抓取页面的链接
-                        ArrayList<Element> links = doc.select("a");
-                        for (Element aTag : links) {
-                            linkPool.add(aTag.attr("href"));
-                        }
-                        //假如这是一个新闻页面，就存入数据库，否则，就什么都不做
-                        ArrayList<Element> articleTags = doc.select("article");
-                        if (!articleTags.isEmpty()) {
-//                            for (Element articleTag : articleTags) {
-                            System.out.println(articleTags.get(0).child(0).text());
-//                            }
-                        }
-                        processedLinks.add(finalLink);
-                        return null;
-                    });
-                } catch (IOException e) {
-                    throw new RuntimeException(e);
+            if (isInterestingPage(link)) {
+                Document doc = httpGetAndParseHtml(link);
+                // 获取抓取页面的链接
+                ArrayList<Element> links = doc.select("a");
+                for (Element aTag : links) {
+                    linkPool.add(aTag.attr("href"));
                 }
+                storeIntoDatabaseIfItIsNewsPage(doc);
+                processedLinks.add(link);
             }
         }
 
+    }
+
+    private static void storeIntoDatabaseIfItIsNewsPage(Document doc) {
+        ArrayList<Element> articleTags = doc.select("article");
+        if (!articleTags.isEmpty()) {
+            System.out.println(articleTags.get(0).child(0).text());
+        }
+    }
+
+    private static Document httpGetAndParseHtml(String link) {
+        try (CloseableHttpClient httpclient = HttpClients.createDefault()) {
+            ClassicHttpRequest httpGet = ClassicRequestBuilder.get(link)
+                    .addHeader("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36")
+                    .build();
+            return httpclient.execute(httpGet, response -> {
+                final HttpEntity entity = response.getEntity();
+                String html = EntityUtils.toString(entity);
+                return Jsoup.parse(html);
+            });
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static boolean isInterestingPage(String link) {
+        return (isNewsPage(link) && isNotLoginPage(link)) || isHomePage(link);
+    }
+
+    private static boolean isNewsPage(String link) {
+        return link.contains("news.sina.cn");
+    }
+
+    private static boolean isNotLoginPage(String link) {
+        return !link.contains("passport.sina.cn");
+    }
+
+    private static boolean isHomePage(String link) {
+        return "https://sina.cn/".equals(link);
     }
 }
