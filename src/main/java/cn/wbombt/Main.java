e859fa9fd0acb4043d93afb5f01d206af8bca037
George <jobsteng@163.com>
2023-06-07 15:32:23 +0800

    insert link to database

--- src/main/java/cn/wbombt/Main.java
+++ src/main/java/cn/wbombt/Main.java
@@ -15,9 +15,7 @@ import java.io.File;
 import java.io.IOException;
 import java.sql.*;
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 /**
  * @author George
@@ -27,35 +25,37 @@ public class Main {
         //链接数据库
         File projectDir = new File(System.getProperty("basedir", System.getProperty("user.dir")));
         String jdbcUrl = "jdbc:h2:file:" + new File(projectDir, "news").getAbsolutePath();
-//        System.out.println(jdbcUrl);
+
         try (Connection connection = DriverManager.getConnection(jdbcUrl)) {
-            // 待处理的链接池
-            List<String> linkPool = loadUrlFromDatabase(connection, "SELECT link from LINKS_TO_BE_PROCESSED");
-            //已处理过的连接池
-            Set<String> processedLinks = new HashSet<>(loadUrlFromDatabase(connection, "SELECT link from LINKS_ALREADY_PROCESSED"));
-            linkPool.add("https://sina.cn/");
-            String[] s = {};
-            float f = 1.1f;
-            short s1 = 1;
-            while (!linkPool.isEmpty()) {
-                //remove()函数返回被删除的元素
+            while (true) {
+                List<String> linkPool = loadUrlFromDatabase(connection, "SELECT link from LINKS_TO_BE_PROCESSED");
+                if (linkPool.isEmpty()) {
+                    break;
+                }
+                //remove()函数返回被删除的元素,同时删除数据库数据
                 String link = linkPool.remove(linkPool.size() - 1);
+                updateData(connection, link, "DELETE FROM LINKS_TO_BE_PROCESSED WHERE link = ?");
                 //链接正确性检查
                 if (link.startsWith("//")) {
                     link = "https:" + link;
                 }
-                if (processedLinks.contains(link)) {
+                if (isLinkProcessed(connection, link)) {
                     continue;
                 }
                 if (isInterestingPage(link)) {
                     Document doc = httpGetAndParseHtml(link);
-                    // 获取页面的链接
-                    doc.select("a").stream().map(aTag -> aTag.attr("href")).forEach(linkPool::add);
+                    parseUrlsFromPageAndStoreIntoDatabase(connection, doc);
                     storeIntoDatabaseIfItIsNewsPage(doc);
-                    processedLinks.add(link);
+                    updateData(connection, link, "insert into LINKS_ALREADY_PROCESSED (LINK) values (?)");
                 }
             }
         }
+    }
+
+    private static void parseUrlsFromPageAndStoreIntoDatabase(Connection connection, Document doc) {
+        doc.select("a").stream().map(aTag -> aTag.attr("href")).forEach(href -> {
+            updateData(connection, href, "insert into LINKS_TO_BE_PROCESSED (LINK) values (?)");
+        });
     }
 
     private static List<String> loadUrlFromDatabase(Connection connection, String sql) throws SQLException {
@@ -78,9 +78,7 @@ public class Main {
 
     private static Document httpGetAndParseHtml(String link) {
         try (CloseableHttpClient httpclient = HttpClients.createDefault()) {
-            ClassicHttpRequest httpGet = ClassicRequestBuilder.get(link)
-                    .addHeader("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36")
-                    .build();
+            ClassicHttpRequest httpGet = ClassicRequestBuilder.get(link).addHeader("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36").build();
             return httpclient.execute(httpGet, response -> {
                 final HttpEntity entity = response.getEntity();
                 String html = EntityUtils.toString(entity);
@@ -105,5 +103,24 @@ public class Main {
 
     private static boolean isHomePage(String link) {
         return "https://sina.cn/".equals(link);
+    }
+
+    private static void updateData(Connection connection, String link, String sql) {
+        try (PreparedStatement statement = connection.prepareStatement(sql)) {
+            statement.setString(1, link);
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static boolean isLinkProcessed(Connection connection, String link) {
+        try (PreparedStatement statement = connection.prepareStatement("SELECT link FROM LINKS_ALREADY_PROCESSED WHERE link = ?")) {
+            statement.setString(1, link);
+            ResultSet resultSet = statement.executeQuery();
+            return resultSet.next();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
